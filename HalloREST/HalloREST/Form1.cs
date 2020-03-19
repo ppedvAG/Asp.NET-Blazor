50e2775015a926b0b25df33e6a7123f250930052
André <ruland@outlook.de>
2020-03-19 14:15:46 +0100

    Katzen JSON

---
+++ HalloREST/HalloREST/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace HalloREST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            double zahl = 74.474747474;
+            string text = "lala" + zahl + "lala";
+            string text2 = string.Format("lala {0:c} lala", zahl);
+            string text3 = $"lala {zahl:c} lala";
+
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={textBox1.Text}";
+            var http = new HttpClient();
+            var json = await http.GetStringAsync(url);
+
+        }
+    }
+}
